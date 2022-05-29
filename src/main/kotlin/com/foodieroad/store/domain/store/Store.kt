e5b5fec81edbce510e6dd19e53010d5d65c5e25c
kimsangjae <zzai_sang@naver.com>
2022-05-29 22:28:06 +0900

    entity 추가

--- src/main/kotlin/com/foodieroad/store/domain/store/Store.kt
+++ src/main/kotlin/com/foodieroad/store/domain/store/Store.kt
@@ -1,14 +1,18 @@
 package com.foodieroad.store.domain.store
 
 import org.springframework.data.annotation.Id
+import javax.persistence.Entity
 
 /**
  * @author zzai_sang
  * @version 0.1.0
  * @since 2022/05/21
  */
+@Entity
 class Store (
-    @Id()
+    @javax.persistence.Id
     var id:Long
         ){
+
+
 }
